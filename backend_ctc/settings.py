8ef57c4346e8a7e32c41c4ade42a8fa23e6cd509
ngdev07 <nglamembrane@gmail.com>
2026-08-25 04:11:07 +0100

    Remove Windows dependencies

--- backend_ctc/settings.py
+++ backend_ctc/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
